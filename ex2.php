8955d2e85e251a314a369e155da0621f072c5659
bominz <bominz147@gmail.com>
2024-12-17 13:24:53 +0700

    Ex2

--- ex2.php
+++ ex2.php
@@ -1,25 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!--  -->
+    <!-- เพิ่ม font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Mitr:wght@200;300;400;500;600;700&family=Prompt:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-        <style>
-            body{
-                font-family: "Mitr", sans-serif;
-                font-weight: 400;
-                font-style: normal;
-            }
-        </style>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    
+    <style>
+    body{
+        font-family: "K2D", sans-serif;
+        font-weight: 600;
+        font-style: normal;
+        margin: right 100px; ;
+    }
+
+    </style>
+    <!-- สิ้นสุดเพิ่ม font -->
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ตัวอย่าง 1</title>
 </head>
-<body>  
-    <h1> โปรแกรม ภาษา HTML</h1>
-    664485010 นายประกฤษฎิ์ ปลัดโส <br>
+<body>
+    <h1>โปรแกรม ภาษา PHP </h1>
+    664485010 ประกฤษฎิ์ ปลัดโส <br>
     หมู่เรียน 66/96
 
+    <?php
+        echo "<h2>สวัสดีปีใหม่</h2>";
+        echo "<br>ขอให้ทุกคนมีความสุข";
+        $name="ประกฤษฎิ์";
+        $surname="ปลัดโส";
+        echo "<br>ชื่อของคุณคือ".$name." ".$surname;
+        $x=15;
+        $y=7;
+        $z=$x+$y;
+        echo "<br>ค่าผลบวกเท่ากับ".$z;
+        $x=15;
+        $y=7;
+        $z=$x*$y;
+        echo "<br>ค่าผลคูณเท่ากับ".$z;
+    ?>
 </body>
 </html>
---
+++ ex4.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Charmonman:wght@400;700&family=Noto+Sans+Thai+Looped:wght@100;200;300;400;500;600;700;800;900&family=Srisakdi:wght@400;700&display=swap" rel="stylesheet">
+
+    <style>
+        body{
+            font-family: "Noto Sans Thai Looped", sans-serif;
+            font-weight: 700;
+            font-style: normal;
+                
+        }
+
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ตัวอย่าง 1 </title>
+</head>
+<body>
+    <h1>โปรแกรม ภาษา HTML สร้างฟอร์มข้อมูล</h1>
+    664485010 นายประกฤษฎิ์ ปลัดโส <br>
+    หมู่เรียน66/96 <br>
+    <?php
+    $name=$_GET['n'];
+    $surname=$_GET['s'];
+    echo' My name is'.$name.''.$surname;
+
+    ?>
+</body>
+</html>
